bd7a88b355f5b6410b404c351a33526eaa5008c6
PGuihen <paul.guihen@gmail.com>
2014-02-18 20:39:40 +0000

    rad 302 started

---
+++ Trip_booking/Trip_booking/DAL/BookingContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+
+namespace Trip_booking.DAL
+{
+    public class BookingContext :DbContext 
+    {
+        public DbSet<Trips> Trips { get; set; }
+        public DbSet<Legs> legs { get; set; }
+        public DbSet<Start_Date> Start_Date { get; set; }
+        public DbSet<Finish_Date> Finish_Date { get; set; }
+    }
+    
+    public BookingContext ()
+        : base ("Booking_DataBase")
+        {
+            Database.SetInitializer(new BookingInitializer());
+            }
+ protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+}
---
+++ Trip_booking/Trip_booking/Models/Trips.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+namespace Trip_booking.Models
+{
+    public class Trips
+    {
+        public int TripID { get; set; }
+        public string Trip_Name { get; set; }
+        public int LegID { get; set; }
+        public  DateTime Start_Date { get; set; }
+        public DateTime Finish_Date { get; set; }
+        // [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+
+
+        public ICollection<Trips> Trips { get; set; }
+    }
+
+    public class Legs
+    {
+        public int LegID { get; set; }
+        public string Start_location { get; set; }
+        public string Finish_location { get; set; }
+        public DateTime Start_Date { get; set; }
+        public DateTime Finish_Date { get; set; }
+        
+    }
+
+    public class Guest
+    {
+        public string Guest_Name { get; set; }
+        public int LegID { get; set; }
+        
+
+    }
+
+
+
+
+}
