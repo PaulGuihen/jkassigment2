9668b431d29effce93f44f01a87f81e2643d0987
PGuihen <paul.guihen@gmail.com>
2014-02-28 13:35:15 +0000

    bootstrap added n working

--- Trip_booking/Trip_booking/App_Start/BundleConfig.cs
+++ Trip_booking/Trip_booking/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace Trip_booking
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+     "~/Scripts/bootstrap*"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
