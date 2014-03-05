70802aba55b56ae2ef4421d5c9d564ef81116902
PGuihen <paul.guihen@gmail.com>
2014-03-05 23:00:17 +0000

    validation on start date and end date  n styling

--- Trip_booking/Trip_booking/Controllers/HomeController.cs
+++ Trip_booking/Trip_booking/Controllers/HomeController.cs
@@ -47,6 +47,9 @@ namespace Trip_booking.Controllers
             return View();
         }
 
+
+
+
         // POST: /Student/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -54,6 +57,8 @@ namespace Trip_booking.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Trip_Name,LegID,Start_Date,Finish_Date")] Trips trips)
         {
+
+            
             if (ModelState.IsValid)
             {
                 _ctx.Trips.Add(trips);
@@ -88,20 +93,70 @@ namespace Trip_booking.Controllers
 
             return View(guest);
         }
+        // populate legs trips drop down list with data frm trips table
+        private void PopulateTripsDropDownList(object selectedTeams = null)
+        {
+            var tripsQuery = from d in _ctx.Trips
+                             orderby d.Trip_Name
+                             select d;
+            ViewBag.TripID = new SelectList(tripsQuery, "TripID", "Trip_Name", selectedTeams);
+        }
 
-        // GET: /Student/Create
+        
+        // GET: /trips/Create
         public ActionResult CreateLegs()
         {
+            PopulateTripsDropDownList();
             return View();
         }
 
         // POST: /Student/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
+        
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult CreateLegs([Bind(Include = "Start_location,Finish_location,Start_Date,Finish_Date,TripID")] Legs legs)
         {
+           // Using the legs.TirpID get the matching trip from the Trips table
+            int nLegTripID = legs.TripID;
+            var TripQyery = from t in _ctx.Trips where t.TripID == nLegTripID select t;
+
+            // Validate the Legs start and finish date agains the Trip start and Finish date
+            foreach (var trip in TripQyery)
+            {
+                DateTime tripStartDate = trip.Start_Date;
+                DateTime tripEndDate = trip.Finish_Date;
+
+                // Legs start date must be greater then Trips.StartDate
+                if (tripStartDate > legs.Start_Date)
+                {
+                    ModelState.AddModelError("Start_Date", "Leg Start Date must not start before: " + tripStartDate.ToString());
+                }
+
+                //  Leg start date must not be after the trip end date
+                if (tripEndDate < legs.Start_Date)
+                {
+                    ModelState.AddModelError("Start_Date", "Leg Start Date must not end after: " + tripEndDate.ToString());
+                }
+
+                // leg finish date must not be after the trip end date
+                if (tripEndDate < legs.Finish_Date)
+                {
+                    ModelState.AddModelError("Finish_Date", "Leg Finish Date must not end after: " + tripEndDate.ToString());
+
+                }
+
+                // leg finish date canno be before trip start date
+                if (tripStartDate > legs.Finish_Date)
+                {
+                    ModelState.AddModelError("Finish_Date", "Leg Finish Date must not end before: " + tripStartDate.ToString());
+
+                }
+            }
+
             if (ModelState.IsValid)
             {
                 _ctx.Legs.Add(legs);
